1aa73db39efcbcd9dd5355283f95a1206a0d5caf
Surit Aryal <surit_aryal@yahoo.com>
2017-08-02 15:49:39 +0545

    Add files via upload

---
+++ rules.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+iptables -I INPUT 1 -i lo -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+val=`iptables -L FORWARD --line-numbers | grep "NETWORK_STATS" | tail -1 | cut -d' ' -f1`
+val1=`expr 1 + $val`
+iptables -I FORWARD $val1 -m state --state RELATED,ESTABLISHED -j ACCEPT
