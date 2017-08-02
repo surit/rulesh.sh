b3ec23c969ce3b5066ca4225187ec1bb4f72ab70
Surit Aryal <surit_aryal@yahoo.com>
2017-08-02 17:47:35 +0545

    Update rules.sh

--- rules.sh
+++ rules.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 iptables -I INPUT 1 -i lo -j ACCEPT
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 val=`iptables -L FORWARD --line-numbers | grep "NETWORK_STATS" | tail -1 | cut -d' ' -f1`
